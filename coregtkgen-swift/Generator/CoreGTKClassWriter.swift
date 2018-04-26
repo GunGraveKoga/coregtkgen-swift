d73373e9b8003829f5bbddd3214a8c6ca38d8e52
GunGraveKoga <vovk.yury@me.com>
2018-04-26 03:29:08 +0300

    Generator implementation

--- coregtkgen-swift/Generator/CoreGTKClassWriter.swift
+++ coregtkgen-swift/Generator/CoreGTKClassWriter.swift
@@ -7,3 +7,178 @@
 //
 
 import Foundation
+
+fileprivate let configURL = URL(fileURLWithPath: CommandLine.arguments[2], isDirectory: true)
+
+public enum CoreGTKClassWriter {
+    public static func generateFiles(forClass gtkClass: CoreGTKClass, inDirectory directory: String) throws {
+        var isDir: ObjCBool = false
+        
+        if FileManager.default.fileExists(atPath: directory, isDirectory: &isDir) && isDir.boolValue {
+            let directoryURL = URL(fileURLWithPath: directory, isDirectory: true)
+            
+            let fileURL = directoryURL.appendingPathComponent(gtkClass.name, isDirectory: false).appendingPathExtension("swift")
+            
+            try self.sourceString(forClass: gtkClass).write(to: fileURL, atomically: false, encoding: .utf8)
+            
+        } else {
+            var message = "Invalid output directory path \(directory)"
+            
+            if !isDir.boolValue {
+                message += " (not directory!)"
+            }
+            
+            print(message)
+            fatalError(message)
+        }
+    }
+    
+    public static func sourceString(forClass gtkClass: CoreGTKClass) -> String {
+        var output = ""
+        
+        output += self.generateLicense(forFile: "\(gtkClass.name).swift") ?? ""
+        output += "\nimport CGTK\n\n"
+        
+        output += "open class \(gtkClass.name) : \(CoreGTKUtil.swapTypes(gtkClass.cParentType!)) {\n"
+        
+        for function in gtkClass.functions {
+            output += "\topen class "
+            output += self.sourceString(forFunction: function)
+        }
+        
+        let _extraMethods = CoreGTKUtil.extraMethods(of: gtkClass.name)
+        
+        if let extraMethods = _extraMethods {
+            for (key, value) in extraMethods {
+                output += "\(key)\n\(value as! String)\n\n"
+            }
+        }
+        
+        for constructor in gtkClass.constructors {
+            output += self.sourceString(forConstructor: constructor, of: gtkClass)
+        }
+        
+        for method in gtkClass.methods {
+            output += "\topen "
+            
+            output += self.sourceString(forFunction: method)
+        }
+        
+        output += "}\n"
+        
+        return output
+    }
+    
+    public static func sourceString(forFunction gtkFunction: CoreGTKMethod) -> String {
+        var output = "func \(gtkFunction.sig)"
+        
+        if !gtkFunction.returnsVoid {
+            output += " -> \(gtkFunction.returnType)"
+        }
+        
+        output += " {\n"
+        
+        if gtkFunction.returnsVoid {
+            output += "\t\t\(gtkFunction.cName)(\(self.generateCParameterListString(gtkFunction.parameters)))\n"
+        } else {
+            output += "\t\treturn "
+            let name = "\(gtkFunction.cName)(\(self.generateCParameterListString(gtkFunction.parameters)))"
+            
+            if CoreGTKUtil.isTypeSwappable(gtkFunction.cReturnType) {
+                output += CoreGTKUtil.convertType(from: gtkFunction.cReturnType, to: gtkFunction.returnType, withName: name)
+            } else {
+                output += name
+            }
+            
+            output += "\n"
+        }
+        
+        output += "\t}\n\n"
+        
+        return output
+    }
+    
+    public static func sourceString(forConstructor constructor: CoreGTKMethod, of gtkClass: CoreGTKClass) -> String {
+        var output = ""
+        
+        output += "\topen init() {\n\t\t"
+        let constructorString = "\(constructor.cName)(\(self.generateCParameterListString(constructor.parameters)))"
+        
+        output += CoreGTKUtil.getFunctionCallForConstructor(of: gtkClass.cType, with: constructorString)
+        
+        output += "\t}\n\n"
+        
+        return output
+    }
+    
+    public static func generateCParameterListString(_ params: [CoreGTKParameter]) -> String {
+        var paramsOutput = ""
+        let limit = params.count - 1
+        
+        for i in 0..<params.count {
+            let param = params[i]
+            
+            paramsOutput += CoreGTKUtil.convertType(from: param.type, to: param.cType, withName: param.name)
+            
+            if i < limit {
+                paramsOutput += ", "
+            }
+        }
+        
+        return paramsOutput
+    }
+    
+    public static func generateCParameterListWith(instance instanceType: String, params: [CoreGTKParameter]) -> String {
+        var paramsOutput = ""
+        
+        paramsOutput += CoreGTKUtil.selfTypeMethodCall(instanceType)
+        
+        if params.count > 0 {
+            paramsOutput += ", "
+            
+            let limit = params.count - 1
+            for i in 0..<params.count {
+                let param = params[i]
+                
+                paramsOutput += CoreGTKUtil.convertType(from: param.type, to: param.cType, withName: param.name)
+                
+                if i < limit {
+                    paramsOutput += ", "
+                }
+            }
+        }
+        
+        return paramsOutput
+    }
+    
+    public static func generateLicense(forFile fileName: String) -> String? {
+        do {
+            let licText = try String(contentsOf: configURL.appendingPathComponent("license.txt"))
+            
+            return licText.replacingOccurrences(of: "@@@FILENAME@@@", with: fileName)
+            
+        } catch let error {
+            print("Error reading license file: \(error)")
+            
+            return nil
+        }
+    }
+    
+    public static func generateDocumentation(forMethod method: CoreGTKMethod) -> String {
+        var doc = "/// func \(method.sig) -> \(method.returnType)\n"
+        
+        if method.parameters.count > 0 {
+            doc += "/// Parameters:\n"
+            
+            for parameter in method.parameters {
+                doc += "///\t- \(parameter.name): \(parameter.type)\n"
+            }
+        }
+        
+        if !method.returnsVoid {
+            doc += "/// - Returns: \(method.returnType)"
+        }
+        
+        return doc
+    }
+}
--- coregtkgen-swift/Generator/CoreGTKUtil.swift
+++ coregtkgen-swift/Generator/CoreGTKUtil.swift
@@ -8,9 +8,24 @@
 
 import Foundation
 
+fileprivate let configURL = URL(fileURLWithPath: CommandLine.arguments[2], isDirectory: true)
 fileprivate var trimMethodNames = [String]()
+fileprivate var dictConvertType = [String: Any]()
+fileprivate var dictGlobalConf = [String: Any]()
+fileprivate var dictSwapTypes = [String: Any]()
+fileprivate var dictExtraImports = [String: Any]()
+fileprivate var dictExtraMethods = [String: Any]()
+
+
 
 public enum CoreGTKUtil {
+    fileprivate static func _dictionaryFromFile(_ file: String) -> [String: Any] {
+        let url = configURL.appendingPathComponent(file, isDirectory: false)
+        let data = try! Data(contentsOf: url)
+        let jsonDecoder = JSONDecoder()
+        
+        return try! jsonDecoder.decode(Dictionary<String, Any>.self, from: data)
+    }
     
     public static func convertUSSToCamelCase(_ string: String) -> String {
         let output = self.convertUSSToCapCase(string)
@@ -28,31 +43,110 @@ public enum CoreGTKUtil {
     }
     
     public static func convertUSSToCapCase(_ string: String) -> String {
+        var output = ""
+        var previousItemWasSingleChar = false
         
+        for component in string.split(separator: "_") {
+            if component.count > 1 {
+                if previousItemWasSingleChar {
+                    output += component
+                } else {
+                    let index = component.index(after: component.startIndex)
+                    output += component[..<index].uppercased()
+                    output += component[index...]
+                }
+                
+            } else {
+                output += component.uppercased()
+                previousItemWasSingleChar = true
+            }
+        }
+        
+        return output
     }
     
     public static func isTypeSwappable(_ type: String) -> Bool {
-        
+        return type == "Array" || self.swapTypes(type) != type
     }
     
     public static func swapTypes(_ type: String) -> String {
+        if dictSwapTypes.count == 0 {
+            dictSwapTypes.merge(self._dictionaryFromFile("swap_types.map"), uniquingKeysWith: {(_, last) in last})
+        }
         
-    }
-    
-    public static func convertFunctionToInit(_ function: String) -> String {
+        let val = dictSwapTypes[type]
         
+        return (val != nil) ? (val as! String) : type
     }
     
     public static func getFunctionCallForConstructor(of type: String, with constructor: String) -> String {
-        
+        return "super.init(withGObject: \(constructor))"
     }
     
     public static func convertType(from fromType: String, to toType: String, withName name: String) -> String {
+        if dictConvertType.count == 0 {
+            dictConvertType.merge(self._dictionaryFromFile("convert_type.map"), uniquingKeysWith: {(_, last) in last})
+        }
         
+        let outerDict = dictConvertType[fromType]
+        
+        if outerDict == nil {
+            if fromType.hasPrefix("Gtk") && toType.hasPrefix("CGTK") {
+                return "\(toType[..<toType.endIndex])(withGObject: \(name))"
+            } else if fromType.hasPrefix("CGTK") && toType.hasPrefix("Gtk") {
+                let index = toType.index(toType.startIndex, offsetBy: 4)
+                return "\(name).\(toType[index...])"
+            } else {
+                return name
+            }
+        }
+        
+        let val = (outerDict as! Dictionary<String, Any>)[toType]
+        
+        if val == nil {
+            return name
+        } else {
+            return String(format: (val as! String), name)
+        }
     }
     
     public static func selfTypeMethodCall(_ type: String) -> String {
-        
+        if type.hasPrefix("CGTK") {
+            let swappedType = self.swapTypes(type)
+            let index = swappedType.index(swappedType.startIndex, offsetBy: 3)
+            
+            return "self.\(swappedType[index...])"
+        } else if type.hasPrefix("Gtk") {
+            var result = ""
+            
+            if type == "GtkGLArea" {
+                result += "GTK_GL_AREA"
+            } else {
+                var countBetweenUnderscores = 0
+                var i = 0
+                let set = CharacterSet.uppercaseLetters
+                
+                type.unicodeScalars.forEach {
+                    
+                    if i != 0 && set.contains($0) && countBetweenUnderscores > 0 {
+                        result += "_\(String($0).uppercased())"
+                        countBetweenUnderscores = 0
+                    } else {
+                        result += String($0).uppercased()
+                        countBetweenUnderscores += 1
+                    }
+                    
+                    i += 1
+                }
+            }
+            
+            result += "(self.GOBJECT)"
+            
+            return result
+            
+        } else {
+            return type
+        }
     }
     
     public static func addToTrimMethodName(_ value: String) {
@@ -62,18 +156,48 @@ public enum CoreGTKUtil {
     }
     
     public static func trimMethodName(_ method: String) -> String {
+        var longestMatch: String? = nil
         
+        for element in trimMethodNames {
+            if method.hasPrefix(element) {
+                if longestMatch == nil {
+                    longestMatch = element
+                } else if longestMatch!.count < element.count {
+                    longestMatch = element
+                }
+            }
+        }
+        
+        if let longestMatch = longestMatch {
+            let range = method.range(of: longestMatch)
+            
+            return String(method[range!.upperBound...])
+        }
+        
+        return method
     }
     
-    public static func extraImports(of clazz: String) -> [String] {
+    public static func extraImports(of clazz: String) -> [String]? {
+        if dictExtraImports.count == 0 {
+            dictExtraImports.merge(self._dictionaryFromFile("extra_imports.map"), uniquingKeysWith: {(_, last) in last})
+        }
         
+        return dictExtraImports[clazz] as? Array<String>
     }
     
-    public static func extraMethods(of clazz: String) -> [String: Any] {
+    public static func extraMethods(of clazz: String) -> [String: Any]? {
+        if dictExtraMethods.count == 0 {
+            dictExtraMethods.merge(self._dictionaryFromFile("extra_methods.map"), uniquingKeysWith: {(_, last) in last})
+        }
         
+        return dictExtraMethods[clazz] as? Dictionary<String, Any>
     }
     
-    public static func globalConfigValue(forKey key: String) -> Any {
+    public static func globalConfigValue(forKey key: String) -> Any? {
+        if dictGlobalConf.count == 0 {
+            dictGlobalConf.merge(self._dictionaryFromFile("global_conf.map"), uniquingKeysWith: {(_, last) in last})
+        }
         
+        return dictGlobalConf[key]
     }
 }
